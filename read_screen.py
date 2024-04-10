6067f2736249c666b4b90f265ccfbbb3479e465e
baozidai <baozidai@outlook.com>
2024-04-10 14:11:57 +0800

    现在可以显示示波器屏幕的副本 (CH1)

--- read_screen.py
+++ read_screen.py
@@ -10,22 +10,29 @@ scope:pyvisa.resources.Resource = rm.open_resource('TCPIP::192.168.1.55::INSTR')
 
 # 设置读取模式
 scope.write(":WAV:SOUR CHAN1")
-scope.write(":WAVeform:mode normal")
+scope.write(":WAVeform:mode normal") # 读取屏幕上的数据
 scope.write(":WAVeform:form ascii")
+scope.write(":WAVeform:POINts 1000") # mode 为normal的时候这个值会被忽略
+
+# 查询X上两点时间间隔
+scope.write(":WAVeform:XINCrement?")
+time_interval=float(scope.read())
 
 # 读取
 scope.write(":WAVeform:data?")
 data=scope.read()
-data=data.split(",")
-data.pop()
-y_data = np.array(data)
+metadata_ascii = data[:12]
+wave_data_ascii = data[12:]
+wave_data=wave_data_ascii.split(",")
+wave_data.pop()
+y_data = np.array(wave_data,dtype="float32")
 # 对y_data进行滤波
 
 
 # 创建一个时间数组，假设每个点之间的时间间隔是恒定的
 # 这里我们假设采样率为1000点/秒，并且波形长度为num_points
 sampling_rate = 1000  # 采样率，单位：点/秒
-time_data = np.arange(0, 1000) / sampling_rate  # 时间数组，单位：秒
+time_data = np.arange(0, len(wave_data)) / (1/time_interval)  # 时间数组，单位：秒
 print(y_data)
 print(time_data)
 
@@ -33,7 +40,7 @@ print(time_data)
 plt.figure(figsize=(10, 5))
 plt.plot(time_data, y_data, label='Oscilloscope Trace')
 plt.xlabel('Time (s)')
-plt.ylabel('Amplitude (Units)')
+plt.ylabel('Amplitude (V)')
 plt.title('Oscilloscope Waveform')
 plt.grid(True)
 plt.legend()
